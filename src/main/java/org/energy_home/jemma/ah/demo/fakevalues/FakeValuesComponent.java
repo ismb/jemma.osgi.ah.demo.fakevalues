6721d9252f3dd7b4f35a5bd3c6631d34fce3735d
Riccardo Tomasi <riccardo.to@gmail.com>
2015-08-03 15:44:40 +0200

    formatting

--- src/main/java/org/energy_home/jemma/ah/demo/fakevalues/FakeValuesComponent.java
+++ src/main/java/org/energy_home/jemma/ah/demo/fakevalues/FakeValuesComponent.java
@@ -23,128 +23,115 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FakeValuesComponent extends HttpServlet implements ManagedService {
-	
-	private final static Logger log=LoggerFactory.getLogger(FakeValuesComponent.class);
-	private final static String fakeDataServletAlias="/fakevalues";
-	
-	
+
+	private final static Logger log = LoggerFactory.getLogger(FakeValuesComponent.class);
+	private final static String fakeDataServletAlias = "/fakevalues";
+
 	private ConfigurationAdmin configurationAdmin;
 	private HttpService httpService;
 	private ComponentContext componentContext;
 
 	private Dictionary<String, ?> props;
-	
-	public void activate(ComponentContext context)
-	{
-		this.componentContext=context;
-		
-		Dictionary serviceProps=new Hashtable();
-		serviceProps.put(Constants.SERVICE_PID,getServicePID());
+
+	public void activate(ComponentContext context) {
+		this.componentContext = context;
+
+		Dictionary serviceProps = new Hashtable();
+		serviceProps.put(Constants.SERVICE_PID, getServicePID());
 		this.componentContext.getBundleContext().registerService(ManagedService.class.getName(), this, serviceProps);
-		
+
 		try {
 			httpService.registerServlet(fakeDataServletAlias, this, null, null);
-		} catch (Exception e) {	
-			log.error("Unable to register servlet for fake values",e);
+		} catch (Exception e) {
+			log.error("Unable to register servlet for fake values", e);
 		}
 	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	{
-		
-		 response.setContentType("application/json");
-		
-		String responseString=null;
-		if(props==null)
-		{
-			responseString="{}";
-		}else{
-			 responseString="{";
-			 for (Enumeration e = props.keys(); e.hasMoreElements();) 
-			 {
-				 String key=(String)e.nextElement();
-				 String value=(String) props.get(key);
-				 responseString+="\""+key+"\":\""+value+"\"";
-				 if(e.hasMoreElements())
-				 {
-					 responseString+=",";
-				 }
-			 }
-			 responseString+="}";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+		response.setContentType("application/json");
+
+		String responseString = null;
+		if (props == null) {
+			responseString = "{}";
+		} else {
+			responseString = "{";
+			for (Enumeration e = props.keys(); e.hasMoreElements();) {
+				String key = (String) e.nextElement();
+				String value = (String) props.get(key);
+				responseString += "\"" + key + "\":\"" + value + "\"";
+				if (e.hasMoreElements()) {
+					responseString += ",";
+				}
+			}
+			responseString += "}";
 		}
-		
+
 		try {
 			response.getWriter().write(responseString);
 			response.getWriter().flush();
 		} catch (IOException e) {
 			log.error("Error sending response to the client");
 		}
-		
-				
-	}
-	
-	
-	private String getServicePID() {
-		return  "jemma.osgi.ah.fakevalues";
+
 	}
 
-	public void bindConfigurationAdmin(ConfigurationAdmin configurationAdmin){
-		this.configurationAdmin=configurationAdmin;
+	private String getServicePID() {
+		return "jemma.osgi.ah.fakevalues";
 	}
-	
-	public void unbindConfigurationAdmin(ConfigurationAdmin configurationAdmin){
-		this.configurationAdmin=null;
+
+	public void bindConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+		this.configurationAdmin = configurationAdmin;
 	}
-	
-	public void bindHttpService(HttpService httpService)
-	{
-		this.httpService=httpService;
+
+	public void unbindConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+		this.configurationAdmin = null;
 	}
-	
-	
-	public void unbindHttpService(HttpService httpService)
-	{
-		this.httpService=httpService;
+
+	public void bindHttpService(HttpService httpService) {
+		this.httpService = httpService;
 	}
-	
+
+	public void unbindHttpService(HttpService httpService) {
+		this.httpService = httpService;
+	}
+
 	@Override
 	public void updated(Dictionary conf) throws ConfigurationException {
-		
-		if (conf == null){
+
+		if (conf == null) {
 			// Configuration not present, load from file in bundle
 			Dictionary defaultProps;
 			try {
 				defaultProps = loadPropFile();
-			
+
 				configurationAdmin.getConfiguration(getServicePID()).update(defaultProps);
 			} catch (IOException e) {
 				log.error("Unable to load  noserver.properties file");
 			}
-		}else{
-			//save locally properties
-			this.props=conf;
+		} else {
+			// save locally properties
+			this.props = conf;
 		}
 	}
-	
-	
+
 	private Dictionary loadPropFile() throws IOException {
 
 		// String _path = "noserver.properties";
-		//String _path = System.getProperty("user.home") + File.separator + "noserver.properties";
+		// String _path = System.getProperty("user.home") + File.separator +
+		// "noserver.properties";
 		String _path_recovery = "noserver.properties";
 		BundleContext bc = this.componentContext.getBundleContext();
-			
+
 		URL _url = bc.getBundle().getResource(_path_recovery);
 
-		Properties readProps=new Properties();
- 		InputStream stream = _url.openStream();
+		Properties readProps = new Properties();
+		InputStream stream = _url.openStream();
 		readProps.load(stream);
 		stream.close();
-			
+
 		return readProps;
 
 	}
-	
-
 
 }
